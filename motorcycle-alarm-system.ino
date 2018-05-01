d6aa464f008644bcb0493ed75f1fa6270ef0d22e
Unknown <alxbck1@gmail.com>
2018-05-01 20:03:31 +0200

    Added state functions

--- motorcycle-alarm-system.ino
+++ motorcycle-alarm-system.ino
@@ -2,10 +2,35 @@
 
 
 // States
-State state_disabled(NULL, NULL, NULL);
-State state_armed(NULL, NULL, NULL);
-State state_warn(NULL, NULL, NULL);
-State state_alarm(NULL, NULL, NULL);
+State state_disabled(NULL, &on_state_disabled, NULL);
+State state_armed(&on_state_armed_enter, &on_state_armed, NULL);
+State state_warn(NULL, &on_state_warn, NULL);
+State state_alarm(NULL, &on_state_alarm, NULL);
+
+// State DISABLED
+void on_state_disabled(){
+  
+}
+
+// Pre-State ARMED
+void on_state_armed_enter(){
+  
+}
+
+// State ARMED
+void on_state_armed(){
+  
+}
+
+// State WARN
+void on_state_warn(){
+  
+}
+
+// State ALARM
+void on_state_alarm(){
+  
+}
 
 void setup() {
   // put your setup code here, to run once:
